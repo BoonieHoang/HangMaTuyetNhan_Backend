f011c27b11cda4cd6834f1770056fe2ecce28181
Nguyen Ngan <nguyenngan8324@gmail.com>
2026-06-06 19:55:08 +0700

    fix: add payos_checkout_url to PaymentResource; fix cancel/return URLs

--- app/Http/Resources/PaymentResource.php
+++ app/Http/Resources/PaymentResource.php
@@ -10,13 +10,14 @@ class PaymentResource extends JsonResource
     public function toArray($request)
     {
         return [
-            'id' => $this->id,
-            'payment_method' => $this->payment_method,
-            'status' => $this->status,
-            'amount' => $this->amount,
-            'transfer_content' => $this->transfer_content,
-            'qr_url' => $this->qr_url,
-            'paid_at' => $this->paid_at,
+            'id'                  => $this->id,
+            'payment_method'      => $this->payment_method,
+            'status'              => $this->status,
+            'amount'              => $this->amount,
+            'transfer_content'    => $this->transfer_content,
+            'payos_checkout_url'  => $this->payos_checkout_url,
+            'payos_qr_code'       => $this->payos_qr_code,
+            'paid_at'             => $this->paid_at,
         ];
     }
 }
